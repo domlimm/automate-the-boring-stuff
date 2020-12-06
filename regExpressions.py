220057c9edb960b0badf2859c6a46905d0a55f46
domsterthebot <dominiclimkj3448@gmail.com>
2020-12-06 18:36:41 +0800

    Updated regex.py

--- regExpressions.py
+++ regExpressions.py
@@ -1,6 +1,19 @@
 import re
 
-message = 'Call me at 9876-5432 tomorrow. or at 8765-4321'
+message = 'Call me at +65-9876-5432 tomorrow. or at +65-8765-4321'
 
-phoneNumRegex = re.compile(r'\d\d\d\d-\d\d\d\d')
-print(phoneNumRegex.findall(message))
+phoneNumGrpRegex = re.compile(r'(\+\d\d)-(\d\d\d\d)-(\d\d\d\d)')
+mo = phoneNumGrpRegex.search(message)
+print(mo.group(1), mo.group(2), mo.group(3))
+# +65 9876 5432
+
+phoneNumGrpsRegex = re.compile(r'(\+\d\d)-(\d\d\d\d)-(\d\d\d\d)')
+print(phoneNumGrpsRegex.findall(message))
+# [('+65', '9876', '5432'), ('+65', '8765', '4321')]
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile popped a tire')
+print(mo.group())
+# Batmobile
+print(mo.group(1))
+# mobile
